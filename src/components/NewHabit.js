bc03e48650e59a15d809b45f4d3232fb7907a1d4
MichaelMcGilvray <mikeymcgilvray@gmail.com>
2022-03-24 18:10:07 -0400

    Updated NewHabit style and save functionality

--- src/components/NewHabit.js
+++ src/components/NewHabit.js
@@ -1,81 +1,113 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import { Container, Dropdown, Form, Button, Row, Col } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import Select from 'react-select'
 
 const NewHabit = () => {
+    const lengthUnitOptions = [{value: "Days", label: "Days"},
+    {value: "Weeks", label: "Weeks"},
+    {value: "Months", label: "Months"}];
+
+    const lengthUnitStyle = {
+        option: (provided, state) => ({
+          ...provided,
+          color: state.isSelected ? 'white' : 'black',
+          backgroundColor: state.isSelected ? '#0D6EFD' : 'white'
+        })
+    }
+
+    const [selectedLengthUnit, setSelectedLengthUnit] = useState(0);
+
+    const updateLengthUnit = (e) => {
+        setSelectedLengthUnit(e.value);
+    }
+
+    function saveHabit(e) {
+        // Get data from form
+        const habitName = document.getElementById("habitNameInput").value;
+        const task = document.getElementById("taskInput").value;
+        const frequency = document.getElementById("frequencyInput").value;
+        const length = document.getElementById("lengthInput").value;
+        const lengthUnit = selectedLengthUnit;
+        
+        // Validate input
+        
+
+        // Save habit to database
+        const habit = {
+            habit: habitName,
+            task: task,
+            frequency: frequency,
+            length: length,
+            lengthUnit: lengthUnit
+        }
+
+        // For now just output what will be saved to the database
+        alert(JSON.stringify(habit));
+    }
+
     return (
         <>
-        <Container className="mt-3 mb-3">
-            <Row className="justify-content-sm-center">
-                <Col className="col-sm-2">
-                    <h1 >New Habit</h1>
-                </Col>
-            </Row>
-        </Container>
-        
+        <h1 className="NewHabitHeading">New Habit</h1>
 
         <Container className="NewHabitForm w-50">
             <Form>
-                <Row className="mb-4 justify-content-sm-center">
-                    <Col className="col-lg-2">
-                        <Form.Label>Habit Name</Form.Label> 
+                <Row className="mb-4">
+                    <Col className="col-sm-4">
+                        <Form.Label className="text-right">Habit Name</Form.Label> 
                     </Col>
 
-                    <Col className="col-lg-4">
-                        <Form.Control placeholder="Enter habit" />
-                    </Col>
-                </Row>
-
-                <Row className="mb-4 justify-content-lg-center">
-                    <Col className="col-lg-2">
-                        <Form.Label>Task</Form.Label> 
-                    </Col>
-
-                    <Col className="col-lg-4">
-                        <Form.Control placeholder="Enter task" />
+                    <Col className="col-sm-6">
+                        <Form.Control placeholder="Enter habit" id="habitNameInput"/>
                     </Col>
                 </Row>
 
-                <Row className="mb-4 justify-content-lg-center">
-                    <Col className="col-lg-2">
-                        <Form.Label>Frequency</Form.Label> 
+                <Row className="mb-4">
+                    <Col className="col-sm-4">
+                        <Form.Label className="text-right">Task</Form.Label> 
                     </Col>
 
-                    <Col className="col-lg-2">
-                        <Form.Control placeholder="" />
+                    <Col className="col-sm-6">
+                        <Form.Control placeholder="Enter task" id="taskInput" />
+                    </Col>
+                </Row>
+
+                <Row className="mb-4">
+                    <Col className="col-sm-4">
+                        <Form.Label className="text-right">Frequency</Form.Label> 
                     </Col>
 
-                    <Col className="col-lg-2">
+                    <Col className="col-sm-3">
+                        <Form.Control type="number" id="frequencyInput" />
+                    </Col>
+
+                    <Col className="col-sm-3">
                         <Form.Label>Days</Form.Label> 
                     </Col>
                 </Row>
 
-                <Row className="mb-4 justify-content-lg-center">
-                    <Col className="col-lg-2">
-                        <Form.Label>Length</Form.Label> 
+                <Row className="mb-4">
+                    <Col className="col-sm-4">
+                        <Form.Label className="text-right">Length</Form.Label> 
                     </Col>
 
-                    <Col className="col-lg-2">
-                        <Form.Control placeholder="" />
+                    <Col className="col-sm-3">
+                        <Form.Control type="number" id="lengthInput" />
                     </Col>
 
-                    <Col className="col-lg-2">
-                        <Dropdown>
-                            <Dropdown.Toggle id="dropdown-basic">
-                                Days
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/weeks">Weeks</Dropdown.Item>
-                                <Dropdown.Item href="#/months">Months</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
+                    <Col className="col-sm-3">
+                        <Select id="lengthUnitInput" styles={lengthUnitStyle}
+                        options={lengthUnitOptions}
+                        value={lengthUnitOptions.find(obj => obj.value === selectedLengthUnit)}
+                        onChange={updateLengthUnit}>
+                        </Select>
                     </Col>
                 </Row>
                 
             
-                <Row className="justify-content-sm-center">
-                    <Col className="col-sm-2">
-                        <Button variant="primary" size="lg">
+                <Row>
+                    <Col className="col-sm-12 text-center" >
+                        <Button variant="primary" size="lg" onClick={saveHabit}>
                             Create
                         </Button>
                     </Col>
