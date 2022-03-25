12aabd2779ae656569a211921f131fc682e12763
ccb1139 <calebbergen000@gmail.com>
2022-03-24 20:31:53 -0400

    Added achievement basics

---
+++ src/components/Achievement.js
@@ -0,0 +1,27 @@
+import Image from 'react-bootstrap/Image'
+
+const Achievement = ({donut, badge, title, description}) => {
+  return (
+    //   Whole Card
+    <div className='achv row border border-dark rounded mb-10'>
+        {/* LeftSide */}
+        <div className='achv-info col-lg-4 d-flex align-items-center justify-content-center border' id='achv-progress'>
+            <div className='row d-flex align-items-center'>
+                <div className="col-md col-lg acvh-iholder">
+                    <Image className='col acvh-img' id="achv-donut" src={donut} fluid/>
+                </div>
+                <div className="col-md col-lg acvh-iholder">
+                    <Image className='col acvh-img' id="achv-badge" src={badge} fluid />
+                </div> 
+            </div>
+        </div>
+        {/* Right Side */}
+        <div className='achv-info col-lg-8' id='achv-achv-description'>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Achievement
--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,8 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import Achievement from './Achievement'
+import badgeTmp from '../img/badgePlaceHolder.png'
+import donutPH from '../img/DonutPlaceHolder.png'
+
 const Achievements = () => {
     return (
-        <>
-            <p>*Insert achievements page here*</p>
-        </>
+        <div className='overview col-sm-8 col-lg-8'>
+            <Achievement badge={badgeTmp} donut={donutPH} title="Place Holder 1"
+                description="This is an example of an achievement"/>
+            <Achievement badge={badgeTmp} donut={donutPH} title="Place Holder 2"
+            description="This is an example of an achievement"/>
+        </div>
     );
   }
   
