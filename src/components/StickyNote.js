62b673f4c210de696f2fca83bb47731a85eace13
ccb1139 <calebbergen000@gmail.com>
2022-03-26 22:30:40 -0400

    Updated Wheelspin Page

---
+++ src/components/StickyNote.js
@@ -0,0 +1,11 @@
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+function StickyNote({image, color}) {
+  return (
+    <img src={image} tintColor="red"></img>
+
+  )
+}
+
+export default StickyNote
--- src/components/Wheelspin.js
+++ src/components/Wheelspin.js
@@ -1,20 +1,41 @@
-import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button"
+import StickyNote from "./StickyNote"
+import SNTmp from "../img/StickyNotePlaceHolder.png"
 import 'bootstrap/dist/css/bootstrap.css'
 
 const Wheelspin = () => {
-  return (
-    <div className='container'>
-        <div id='WsMain' className='row'>
-            <div id='Spinner' className='col-md-6 border'>
-                <Button variant="primary">Spin!</Button>{' '}
+    return (
+        <div className='container'>
+            <div id='WsMain' className='row'>
+                <div id='MysterySticky' className='col-md-6 border'>
+                    <div id="pickUnlock" className="col-md-10 border">
+                        <StickyNote image={SNTmp}></StickyNote>
+                        <StickyNote image={SNTmp}></StickyNote>
+                        <StickyNote image={SNTmp}></StickyNote>
+                    </div>
+                </div>
+                <div className='col-md-6 border'>
+                    <div id='Owned-Items' className="row">
+                        <div className="col-sm-12 border" id="Owned-Items-Head">
+                            <h3>Owned Cosmetics</h3>
+                        </div>
+                        <div className="col-sm-12 border">
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                            <StickyNote image={SNTmp}></StickyNote>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
-            <div id='Owned-Items' className='col-md-6 border'>
-                <h3>Owned</h3>
-            </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Wheelspin
