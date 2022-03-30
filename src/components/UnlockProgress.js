d8d20069c795a35efff5bc3b95d1f6a34fe79e9b
ccb1139 <calebbergen000@gmail.com>
2022-03-30 09:47:58 -0400

    Added Circular Progress Bar

--- src/components/UnlockProgress.js
+++ src/components/UnlockProgress.js
@@ -1,16 +1,22 @@
 import { ProgressBar } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import Image from 'react-bootstrap/Image'
+import donut from '../img/DonutPlaceHolder.png'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function UnlockProgress({complete, total}) {
+    const progressMsg = complete + " / " + total
     return (
         <div className='col-sm-10 fixed-bottom border' id='ACH-Unlock-Progress'>
             <div className='row d-flex align-items-center justify-content-center'>
                 <div className='col-sm-10 border'>
-                    <ProgressBar now={(complete/total)*100}></ProgressBar>
+                    <ProgressBar now={(complete/total)*100} label={progressMsg}></ProgressBar>
                 </div>
-                <p className='col-sm-2' id='ACH-Unlock-Info'>
-                    {total - complete} donuts left for an unlock!
-                </p>
+                <div className='col-sm-2 border'>
+                    <p>{total - complete} more donuts for an unlock!</p>
+                </div>
+                
             </div>  
         </div>
         
