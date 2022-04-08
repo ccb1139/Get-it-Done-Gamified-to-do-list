a27ada61ace1ae9f79fe6aef3aeceef7d581496c
ccb1139 <calebbergen000@gmail.com>
2022-04-07 20:43:09 -0400

    msc

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -7,7 +7,9 @@ function Badge({ id, badge_path }) {
     const [scale, setScale] = useState(1.35);
 
     return (
-        <Tilt scale={scale} transitionSpeed={2500}>
+        <Tilt scale={scale} transitionSpeed={2500}
+        glareEnable={true} glareMaxOpacity={0.5}
+        glareColor="#ffffff" glarePosition="bottom" glareBorderRadius="40px">
             <Image className='col acvh-img' id={id}
                 src={require('../img/badges/' + badge_path + "/3.png")}
             />
